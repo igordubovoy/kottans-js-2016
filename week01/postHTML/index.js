f099ee6d12f1203a54f7a74cc23f8ae9ade6e8e5
Igor Dubovoy <ihor.dubovyi@globallogic.com>
2016-05-23 18:05:02 +0300

    test

--- week01/postHTML/index.js
+++ week01/postHTML/index.js
@@ -4,11 +4,12 @@ const PostHTML = require("posthtml");
 const html =
       `
       <em class="col-sm-push-7">Hello world</em>
-      <p class="col-lg-offset-11 super col-sm-push-7">bootstrap class</p>
-      <p class="js-smth">sdfsf</p>
+      <p class="col-lg-offset-11 super col-sm-push-7 superPuper">bootstrap class</p>
+      <p class="col-lg-offset-11 col-sm-push-7 superPuper">bootstrap class</p>
+      <p class="js-smth js-smth js-smth blalbalba col-sm-push-1">sdfsf</p>
       `
 const bootstrapCls = /(?:col)-(?:lg|xs|sm|md)-(?:(?:\d{1,2})|(?:(?:push|pull|offset)-\d{1,2}))/ig
-const bootstrapJSCls = /js-/
+const bootstrapJSCls = /js-/ig
 
 const plugin = tree => tree
   .match({ attrs: { class: true }}, node => {
