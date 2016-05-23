e9af823bce4daaaf89fedae0f7d94a001945321d
Igor Dubovoy <ihor.dubovyi@globallogic.com>
2016-05-23 17:17:36 +0300

    homework2 week 2

--- week01/postHTML/index.js
+++ week01/postHTML/index.js
@@ -4,26 +4,19 @@ const PostHTML = require("posthtml");
 const html =
       `
       <em class="col-sm-push-7">Hello world</em>
-      <p class="col-lg-offset-11 super col-sm-push-7">sdfsf</p>
+      <p class="col-lg-offset-11 super col-sm-push-7">bootstrap class</p>
+      <p class="js-smth">sdfsf</p>
       `
-const bootstrapClasses = /(?:col)-(?:lg|xs|sm|md)-(?:(?:\d{1,2})|(?:(?:push|pull|offset)-\d{1,2}))/ig
+const bootstrapCls = /(?:col)-(?:lg|xs|sm|md)-(?:(?:\d{1,2})|(?:(?:push|pull|offset)-\d{1,2}))/ig
+const bootstrapJSCls = /js-/
 
 const plugin = tree => tree
-  .match({tag:"p"}, node => {
-    let matchClasses = node.attrs.class.match(bootstrapClasses)
-    let nodeClasses = node.attrs.class
-    nodeClasses
-      .split(' ')
-      .forEach(function(elem){
-        matchClasses.forEach(function(matchElem, index) {
-          console.log(matchElem)
-          if (elem === matchElem){
-            nodeClasses.splice(elem, 1)
-            index++;
-          }
-        })
-      })
-    console.log(nodeClasses)
+  .match({ attrs: { class: true }}, node => {
+    node.attrs.class = node.attrs.class.replace(bootstrapCls, '').trim()
+    node.attrs.class = node.attrs.class.replace(bootstrapJSCls, 'data-')
+    if (node.attrs.class.length === 0) {
+      delete node.attrs.class
+    }
     return node
   })
 
