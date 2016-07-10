eb2fb4c39ac52959913671c9a0ac70b1672d48fd
Igor Dubovoy <ihor.dubovyi@globallogic.com>
2016-07-10 13:33:31 +0300

    Promises myMap

---
+++ weak04/map.js
@@ -0,0 +1,22 @@
+Promise.prototype.myMap = function (array, mapper) {
+  var
+    result = [],
+    isRejected = false;
+
+  if (!(array instanceof Array)) {
+    throw new Error;
+  }
+
+  return new Promise((resolve, reject) => {
+    array.forEach(elem => {
+      if (typeof elem.then === 'function') {
+        elem
+          .then(elem => { result.push(mapper(elem)) })
+          .catch(() => { isRejected = true });
+      } else {
+        result.push(mapper(elem));
+      }
+    })
+    isRejected ? reject('REJECTED') : resolve(result);
+  })
+}
